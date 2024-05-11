ed9a65fb864a1b1273bdcd0ce81d637ed079f3ef
tlomiej <bartekstaron@gmail.com>
2024-05-11 07:50:12 +0200

    routing

---
+++ flaskapp.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!Its a life</p>"
+
+@app.route("/about")
+def about():
+    return "<p>about</p>"
+
+
+
+
+if __name__ == '__main__':
+   app.run(debug=True)
