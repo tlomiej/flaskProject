9f211eaf1cfa73b97a6a4c665db5862cac418552
tlomiej <bartekstaron@gmail.com>
2024-05-16 18:31:47 +0200

    account update

--- mainapp/forms.py
+++ mainapp/forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
-
+from flask_login import current_user
 from mainapp.models import User
 
 
@@ -24,6 +25,28 @@ class RegistrationForm(FlaskForm):
     user = User.query.filter_by(username= email.data).first()
     if user:
       raise  ValidationError('That email is taken. Pleas choose another')
+
+class UpdateAccountForm(FlaskForm):
+  username = StringField('Username', validators=[DataRequired(), Length(min=4, max=32)])
+  email = StringField('Email', validators=[DataRequired(), Email()])
+
+  picture = FileField("Update profile picture", validators=[FileAllowed(['jpg', 'png', 'svg'])])
+  submit = SubmitField(
+      'Update'
+  )
+
+  def validate_username(self, username):
+    if username.data != current_user.username:
+      user = User.query.filter_by(username= username.data).first()
+      if user:
+        raise  ValidationError('That username is taken. Pleas choose another')
+
+  def validate_email(self, email):
+    if email.data != current_user.email:
+      user = User.query.filter_by(username= email.data).first()
+      if user:
+        raise  ValidationError('That email is taken. Pleas choose another')
+
 
 
 class LoginForm(FlaskForm):
