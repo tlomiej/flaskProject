2cbb000bc2a4cee15409bf0e9620a06e6f1ae138
tlomiej <bartekstaron@gmail.com>
2024-05-25 19:18:42 +0200

    dynamic forms - count

--- mainapp/initModel.py
+++ mainapp/initModel.py
@@ -7,7 +7,6 @@ from mainapp.models import Forms
 with create_app().app_context():
     db.create_all()
 
-
     #db.session.add(User(username='admin2', email='admi211@example.com', password=bcrypt.generate_password_hash('p@ss4App').decode('utf-8')
 #))
 
