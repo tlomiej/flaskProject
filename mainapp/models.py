b43260b5bac6229d94f46c197ddfbfbc80b8c7d1
tlomiej <bartekstaron@gmail.com>
2024-05-27 13:36:24 +0200

    UX/ UI fix

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 #from itsdangerous import URLSafeTimedSerializer as Serializer
-from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+#from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from mainapp import db, login_manager
 from flask_login import UserMixin
 from flask import current_app
@@ -19,18 +19,19 @@ class User(db.Model, UserMixin):
     forms = db.relationship('Forms', backref='author', lazy=True)
     formsdata = db.relationship('Formsdata', backref='author', lazy=True)
 
-    def get_reset_token(self, expires_sec=1800):
-        s = Serializer(current_app.config['SECRET_KEY'], expires_sec)
-        return s.dumps({'user_id': self.id}).decode('utf-8')
+    #def get_reset_token(self, expires_sec=1800):
+        #s = Serializer(current_app.config['SECRET_KEY'], expires_sec)
+        #return s.dumps({'user_id': self.id}).decode('utf-8')
 
     @staticmethod
     def verify_reset_token(token):
-        s = Serializer(current_app.config['SECRET_KEY'])
-        try:
-            user_id = s.loads(token)['user_id']
-        except:
-            return None
-        return User.query.get(user_id)
+        #s = Serializer(current_app.config['SECRET_KEY'])
+        #try:
+           # user_id = s.loads(token)['user_id']
+        #except:
+        #    return None
+       # return User.query.get(user_id)
+        return None
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
